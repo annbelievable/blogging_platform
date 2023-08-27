8c21c5b08b5348cf79ea5fbf3d790de004d5257d
kokann <kokann_92@hotmail.com>
2023-08-27 22:36:17 +0800

    Further update the middleware

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-    let authHeader = req.headers["authorization"];
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
+    let cookie = req.headers.cookie;
+    if (!cookie || !cookie.startsWith("authentication_status")) {
         res.status(401);
-        console.log("USER NOT AUTHORIZED");
         return res.send("User is not authorized");
     }
 
-    const token = authHeader.split(" ")[1];
+    const token = cookie.split("=")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             res.status(401);
