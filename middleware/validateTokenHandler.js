073dccaf15cad567db12ec47d433f122f6dae530
kokann <kokann_92@hotmail.com>
2023-08-27 22:30:19 +0800

    Update middleware logic

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -2,17 +2,21 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
     let authHeader = req.headers["authorization"];
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-            if (err) {
-                res.status(401);
-                throw new Error("User is not authorized");
-            }
-            req.user = decoded.user;
-            next();
-        });
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        res.status(401);
+        console.log("USER NOT AUTHORIZED");
+        return res.send("User is not authorized");
     }
+
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            res.status(401);
+            return res.send("User is not authorized");
+        }
+        req.user = decoded.user;
+        next();
+    });
 };
 
 module.exports = { validateToken };
